94c24e790e30d8f2302a5a33bbdb93f239de48b6
Kaleb Nekatibeb <kaleb8582@gmail.com>
2026-06-24 15:45:15 +0300

    adding constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+# Window settings
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 700
+WINDOW_TITLE = b"Solar System Simulator"
+
+# Camera settings
+CAMERA_POS = (40, 40, 40)
+CAMERA_TARGET = (0, 0, 0)
+CAMERA_UP = (0, 1, 0)
+
+# Simulation settings
+INITIAL_SIMULATION_SPEED = 1.0
+SPEED_STEP = 0.25
